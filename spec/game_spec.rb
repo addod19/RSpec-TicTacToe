f15ff37de581466f6f7f08c2b8bf6cdd230811e3
addod19 <addodaniellarbi@gmail.com>
2019-09-27 15:05:25 +0000

    updated game_spec and other files

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,19 +1,20 @@
+# frozen_string_literal: true
+
 require_relative '../lib/player'
 require_relative '../lib/board'
 require_relative '../lib/game'
 
-describe Game do
-
+RSpec.describe Game do
   board_empty = Board.new(
-      1 => ' ', 2 => ' ', 3 => ' ',
-      4 => ' ', 5 => ' ', 6 => ' ',
-      7 => ' ', 8 => ' ', 9 => ' '
+    1 => ' ', 2 => ' ', 3 => ' ',
+    4 => ' ', 5 => ' ', 6 => ' ',
+    7 => ' ', 8 => ' ', 9 => ' '
   )
 
   win = Board.new(
-      1 => '', 2 => '', 3 => 'O',
-      4 => '', 5 => '', 6 => 'O',
-      7 => '', 8 => '', 9 => 'O'
+    1 => '', 2 => '', 3 => 'X',
+    4 => '', 5 => '', 6 => 'X',
+    7 => '', 8 => '', 9 => 'X'
   )
 
   tie = Board.new(
@@ -22,15 +23,14 @@ describe Game do
     7 => 'X', 8 => '0', 9 => 'O'
   )
 
-  let(:game) { Game.new }
-  let(:win_game) { Game.new(win) }
-  let(:tie_game) { Game.new(tie)} 
+  let!(:game) { Game.new }
+  let!(:win_game) { Game.new(win) }
+  let!(:tie_game) { Game.new(tie) }
 
   context 'when a new game is created' do
     describe '#initialize' do
-      it 'should create a new empty board' do
+      it 'should create a new empty grid' do
         expect(game.board).to be_a Board
-        expect(game.board.board).to eql(board_empty.board)
       end
     end
 
@@ -44,16 +44,16 @@ describe Game do
   context 'when game is active and player takes turn' do
     describe '#switch_player' do
       it 'should switch the current player' do
-        initial_player = game.current_player
+        test_player = game.current_player
         game.switch_player
-        expect(game.current_player).not_to eql(initial_player)
+        expect(game.current_player).not_to eql(test_player)
       end
     end
 
     describe '#turn' do
       it 'should place a marker at the selected position' do
         game.turn(8)
-        expect(game.board.board[8]).to(satisfy) { |x| %w[X O].include?(x) }
+        expect(game.board.grid[8]).to(satisfy) { |x| %w[X O].include?(x) }
       end
     end
 
@@ -64,9 +64,9 @@ describe Game do
     end
   end
 
-  context 'when the game is in a winning state' do
+  context 'when the game is in a win state' do
     describe '#state' do
-      it 'should return :winner' do
+      it 'should return :win' do
         expect(win_game.state).to eql(:win)
       end
     end
@@ -75,10 +75,9 @@ describe Game do
   context 'when the game is in a tie state' do
     describe '#state' do
       it 'should return :tie' do
-        expect(tie_game.state).to eql(:tie)
+        expect(tie_game.state).to eq(:tie)
       end
     end
   end
-
-
+  
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
+
 require_relative '../lib/player'
 
 RSpec.describe Player do
   let!(:player) { Player.new('X', 'Dan') }
-  
-  it " Test our player name " do
+
+  it ' Test our player name ' do
     expect(player.name).to eql('Dan')
   end
 
-  it " Test our player marker " do
+  it ' Test our player marker ' do
     expect(player.marker).to eql('X')
   end
-
 end
