e845c7588b5ea07defd8ebf2c1a25dc49ef42be8
maya88en <maya88en@gmail.com>
2019-09-27 22:33:29 -0700

    cover initialze method in board class

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,10 +3,23 @@ require_relative '../lib/board'
 require_relative '../lib/game'
 
 RSpec.describe Board do
-    it 'has a 3 x 3 grid' do
-      board = Board.new
-      expect(board.board[0]).to eq 3
-      expect(board.Board).to eq 3
+
+    context 'when a new board is created' do
+      describe '#initialize' do
+        it 'should create a new empty board' do
+          board = Board.new
+          expect(board.grid[0]).to eq "@ " 
+        end
+        it "should create a board with a given grid" do
+          tie = Board.new({
+            1 => 'X', 2 => 'X', 3 => 'O',
+            4 => 'O', 5 => 'X', 6 => 'X',
+            7 => 'X', 8 => '0', 9 => 'O'
+          }
+          )
+          expect(tie.grid[1]).to eq "X"
+        end
+      end
     end
 end
 
