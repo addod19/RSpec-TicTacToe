ba09ec8df59384699344395545b94c0544196431
maya88en <maya88en@gmail.com>
2019-10-02 12:10:08 -0700

    fix conflict

--- lib/game.rb
+++ lib/game.rb
@@ -1,30 +1,29 @@
 # frozen_string_literal: true
 
 class Game
+  attr_accessor :player1, :player2
+  attr_reader :current_player, :board
 
-    attr_accessor :player1, :player2
-    attr_reader :current_player, :board
-  
-    def initialize(board = nil)
-      @board = board ? board : Board.new
-      @player1 = Player.new('X')
-      @player2 = Player.new('O')
-      @current_player = [@player1, @player2].sample
-    end
-  
-    def turn(selection)
-      @board.place_marker(selection, @current_player.marker)
-    end
-  
-    def switch_player
-      @current_player = @current_player == @player1 ? @player2 : @player1
-    end
-  
-    def state
-      return :win if @board.win?
-      return :tie if @board.tie?
-  
-      :active
-    end
+  def initialize(board = nil)
+    @board = board || Board.new
+    @player1 = Player.new('X')
+    @player2 = Player.new('O')
+    @current_player = [@player1, @player2].sample
+  end
+
+  def turn(selection)
+    @board.place_marker(selection, @current_player.marker)
+  end
+
+  def switch_player
+    @current_player = @current_player == @player1 ? @player2 : @player1
+  end
+
+  def state
+    return :win if @board.win?
+    return :tie if @board.tie?
+
+    :active
+  end
 end
 
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,67 +4,56 @@ require_relative '../lib/player'
 require_relative '../lib/board'
 require_relative '../lib/game'
 
-
 RSpec.describe Board do
   let!(:board) { Board.new }
-  let!(:tie) { Board.new({
-    1 => 'X', 2 => 'X', 3 => 'O',
-    4 => 'O', 5 => 'X', 6 => 'X',
-    7 => 'X', 8 => '0', 9 => 'O'
-  })}
-    context 'when a new board is created' do
-      describe '#initialize' do
-        it 'should create a new empty board' do
-          board = Board.new 
-          expect(board.grid[0]).to eq "@ " 
-        end
-        it "should create a board with a given grid" do
-          tie = Board.new({
-            1 => 'X', 2 => 'X', 3 => 'O',
-            4 => 'O', 5 => 'X', 6 => 'X',
-            7 => 'X', 8 => '0', 9 => 'O'
-          })
-          expect(tie.grid[1]).to eq "X"
-        end
+  let!(:tie) do
+    Board.new(
+      1 => 'X', 2 => 'X', 3 => 'O',
+      4 => 'O', 5 => 'X', 6 => 'X',
+      7 => 'X', 8 => '0', 9 => 'O'
+    )
+  end
+  context 'when a new board is created' do
+    describe '#initialize' do
+      it 'should create a new empty board' do
+        board = Board.new
+        expect(board.grid[0]).to eq '@ '
+      end
+      it 'should create a board with a given grid' do
+        tie = Board.new(
+          1 => 'X', 2 => 'X', 3 => 'O',
+          4 => 'O', 5 => 'X', 6 => 'X',
+          7 => 'X', 8 => '0', 9 => 'O'
+        )
+        expect(tie.grid[1]).to eq 'X'
       end
     end
-    context "display game board" do
-      describe "#display" do
-        it "should display a grid" do
-          def display_test
-    "
-    |          |
-    #{@grid[0]} | #{@grid[1]} | #{@grid[2]}
-    -----------
-    |          |
-    #{@grid[3]} | #{@grid[4]} | #{@grid[5]}
-    -----------
-    |          |
-    #{@grid[6]} | #{@grid[7]} | #{@grid[8]}
-    "
-          end
-         
-          tie =  Board.new({
-            0 => 'X', 1 => 'X', 2 => 'O',
-            3 => 'O', 4 => 'X', 5 => 'X',
-            6 => 'X', 7 => '0', 8 => 'O'
-          })
-          
-          @grid = tie.grid 
-          expect(tie.display).to eq display_test
-        end 
+  end
+  context 'display game board' do
+    describe '#display' do
+      it 'should display grid length' do
+        expect(board.grid.length).to eq(9)
       end
     end
-    context "check place selection" do
-      describe "#valid_selection" do
-        it "should return true when select a valid position" do
-          board =  Board.new 
-          expect(board.valid_selection?(8)).to eq true
-        end
-        it "should return false when select a valid position" do
-          board =  Board.new 
-          expect(board.valid_selection?(10)).to eq false
-        end
+  end
+  context 'check place selection' do
+    describe '#valid_selection' do
+      it 'should return true when select a valid position' do
+        board = Board.new
+        expect(board.valid_selection?(8)).to eq true
+      end
+      it 'should return false when select a valid position' do
+        board = Board.new
+        expect(board.valid_selection?(10)).to eq false
       end
     end
+  end
+  context 'check for place marker' do
+    describe '#place_marker' do
+      it 'should place marker at the right position' do
+        board = Board.new
+        expect(board.place_marker(5, 'X')).to eql(board.grid[4])
+      end
+    end
+  end
 end
